b40e8323111791962ec49bc1944dfb36579eb50d
albertofragoso <hola@albertofragoso.com>
2019-08-04 18:49:57 -0500

    Deleting action. Modal is incomplete yet.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,6 +2,7 @@ import React, { useState }  from 'react'
 import { database } from '../utils/firebase'
 import { connect } from 'react-redux'
 import { setComments } from '../actions'
+// import Modal from '../components/Modal'
 import moment from 'moment'
 import toastr from 'toastr'
 
@@ -10,16 +11,13 @@ import './styles/Comment.css'
 const Comment = props => {
 
   const [isEditing, setIsEditing] = useState(false)
-  // const [newComment, setNewComment] = useState('')
+  // const [modal, setModal] = useState(false)
+
+  // const showModal = () => setModal(!modal)
 
   const handleEdit = () => setIsEditing(true)
 
   const handleCancel = () => setIsEditing(false)
-
-  // const handleChange = e => {
-  //   setNewComment({ ...newComment, [e.target.name]: e.target.value })
-  //   console.log(newComment) 
-  // }
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -41,6 +39,17 @@ const Comment = props => {
     props.setComments([...updatedComments, data])
     setIsEditing(false)
   }
+
+  const handleDelete = e => {
+    e.preventDefault()
+
+    database.ref('comments/'+props.comment.id).remove()
+      .then(() => toastr.warning('Your comment has been deleted.'))
+      .catch(() => toastr.error('Something went wrong'))
+
+    const updatedComments = props.comments.filter(comment => comment.id !== props.comment.id)
+    props.setComments([...updatedComments])
+  }
   
   const date = moment(props.comment.date).fromNow()
 
@@ -49,42 +58,46 @@ const Comment = props => {
       {
         !isEditing 
         ? <div className="card">
-              <div className="Comment-cover" style={{ background: `url('${props.comment.image}')` }}>
-                <span>
-                  {props.comment.type === 'friends' && '👨‍👩‍👧‍👦'}
-                  {props.comment.type === 'public' && '🌎'}
-                </span>
-              </div>
-              <div className="card-body">
-                <h6 className="card-title">{props.comment.user} says: </h6>
-                <p className="card-text font-weight-light">{props.comment.comment}</p>
-                {
-                  props.user.email === props.comment.user && 
-                  <div>
-                    <button onClick={handleEdit} className="btn btn-secondary mr-2">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
-                  </div> 
-                }
-              </div>
-              <div className="card-footer text-muted">
-                <small>{date}</small>
-              </div>
-            </div>
-        : <form onSubmit={handleSubmit}>
-            <div className="Comment-cover mb-2" style={{ background: `url('${props.comment.image}')` }}>
+            <div className="Comment-cover" style={{ background: `url('${props.comment.image}')` }}>
               <span>
                 {props.comment.type === 'friends' && '👨‍👩‍👧‍👦'}
                 {props.comment.type === 'public' && '🌎'}
               </span>
             </div>
-            <div className="form-group">
-              <textarea className="form-control" name="newcomment" rows="3" defaultValue={props.comment.comment} required></textarea>
+            <div className="card-body">
+              <h6 className="card-title">{props.comment.user} says: </h6>
+              <p className="card-text font-weight-light">{props.comment.comment}</p>
+              {
+                props.user.email === props.comment.user && 
+                <div>
+                  <button onClick={handleEdit} className="btn btn-secondary mr-2">Edit</button>
+                  <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+                </div> 
+              }
             </div>
-            <button className="btn btn-secondary mr-2">Update</button>
-            <button onClick={handleCancel} className="btn btn-danger">Cancel</button>
-          </form>
-      }
-      </div>
+            <div className="card-footer text-muted">
+              <small>{date}</small>
+            </div>
+          </div>
+      : <form onSubmit={handleSubmit}>
+          <div className="Comment-cover mb-2" style={{ background: `url('${props.comment.image}')` }}>
+            <span>
+              {props.comment.type === 'friends' && '👨‍👩‍👧‍👦'}
+              {props.comment.type === 'public' && '🌎'}
+            </span>
+          </div>
+          <div className="form-group">
+            <textarea className="form-control" name="newcomment" rows="3" defaultValue={props.comment.comment} required></textarea>
+          </div>
+          <button className="btn btn-secondary mr-2">Update</button>
+          <button onClick={handleCancel} className="btn btn-danger">Cancel</button>
+        </form>
+    }
+    {/* <Modal show={modal} close={showModal}>
+        <h1>Are you sure want to delete this comment?</h1>
+        <button className="btn btn-danger">Delete</button>
+    </Modal> */}
+    </div>
   )
 }
 
